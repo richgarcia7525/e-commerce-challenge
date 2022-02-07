8004bebccf8bb0d2e4d77d31a3f9198e6ed6b14d
BITCHARD <richgarcia7525@gmail.com>
2022-02-06 20:59:11 -0600

    setting up all the routes. it will transfer a symbol or rendering of the state of that resources straight to the endpoint or whomever is asking for it. each having certain params to indentify them.

--- fantastic-umbrella-main/Develop/routes/api/category-routes.js
+++ fantastic-umbrella-main/Develop/routes/api/category-routes.js
@@ -11,6 +11,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: ['id', 'category_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+    .then(dbCategoriesData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
