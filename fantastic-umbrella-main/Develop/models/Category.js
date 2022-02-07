ad82b27523d7f490c3881a00dcc0e869f5c110a0
BITCHARD <richgarcia7525@gmail.com>
2022-02-06 18:20:37 -0600

    added the column for category and set it up to not allow null input. made sure it was the primary key and that it had the auto inceremnet lastly in the comlumn creation made sure the data type was set to string.

--- fantastic-umbrella-main/Develop/models/Category.js
+++ fantastic-umbrella-main/Develop/models/Category.js
@@ -2,11 +2,22 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Category extends Model {}
+class Category extends Model { }
 
 Category.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //creating the column for category_name
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
