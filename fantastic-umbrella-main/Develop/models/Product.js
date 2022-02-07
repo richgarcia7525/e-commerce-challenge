691ba0bc33ab2e96088dae7bed9d036d9b4ffc14
BITCHARD <richgarcia7525@gmail.com>
2022-02-06 18:56:58 -0600

    added last two colums for stock and for the category id.

--- fantastic-umbrella-main/Develop/models/Product.js
+++ fantastic-umbrella-main/Develop/models/Product.js
@@ -4,7 +4,7 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
@@ -17,19 +17,37 @@ Product.init(
       primaryKey: true,
       aitoIncrement: true
     },
-  //this will be the column that will hold the product name
+    //this will be the column that will hold the product name
     product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
     //this will be the column for price
     price: {
-      type: DataTypes.DECIMAL(10,2),
+      type: DataTypes.DECIMAL(10, 2),
       allowNull: false,
       validate: {
         isDecimal: true
       }
+    },
+    //creating the column for stock
+    stock: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      defaultValue: 10,
+      validates: {
+        isNumeric: true
+      }
+    },
+    //this will create the column for the category id
+    category_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: "category",
+        key: "id"
+      }
     }
+  },
   {
     sequelize,
     timestamps: false,
