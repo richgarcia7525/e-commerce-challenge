de9559fccc28ad2cf9c02b4428fac52ea37a4069
BITCHARD <richgarcia7525@gmail.com>
2022-02-07 05:19:39 -0600

    i guess i missed 2 sections of the routes.so just went back and finished. maybe it will work now

--- fantastic-umbrella-main/Develop/server.js
+++ fantastic-umbrella-main/Develop/server.js
@@ -3,6 +3,7 @@ const app = express();
 const db = require("./models");
 const routes = require('./routes');
 // import sequelize connection
+const sequelize = require('./config/connection');
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
@@ -11,9 +12,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the 
-db.sequelize.sync().then(() => {
-  app.listen(PORT, () => {
-    console.log(`listening on: http://localhost:$(PORT)`);
+sequelize.sync({force:false}).then(() => {
+  app.listen(PORT, () => console.log(`listening on: http://localhost:$(PORT)`);
 
   });
 });
