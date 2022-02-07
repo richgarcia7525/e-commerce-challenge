315d9763835f25b039f9fa2ddba24c3070c859d0
BITCHARD <richgarcia7525@gmail.com>
2022-02-06 19:33:39 -0600

    created all the columns for the products tag and set up all the params for them.

--- fantastic-umbrella-main/Develop/models/ProductTag.js
+++ fantastic-umbrella-main/Develop/models/ProductTag.js
@@ -2,11 +2,35 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
 
-class ProductTag extends Model {}
+class ProductTag extends Model { }
 
 ProductTag.init(
   {
     // define columns
+    // this will be the column for the id
+    id: {
+      type: DataTypes.INTERGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    //this will be the column for the product id
+    product_id: {
+      type: DataType.INTERGER,
+      references: {
+        model: "product",
+        key: "id"
+      }
+    },
+    // creating of the column for the tag id
+    tag_id: {
+      type: DataTypes.INTERGER,
+      references: {
+        model: "tag",
+        key: "id"
+      }
+    }
+
   },
   {
     sequelize,
